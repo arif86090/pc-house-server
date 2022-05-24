283d8d94942ba25dcd6ee155c8c0b24199227b34
Arif <aharif281@gmail.com>
2022-05-24 11:31:26 +0600

    seconds

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function verifyJwt(req,res,next){
  const token =authheader.split(' ')[1];
    jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded) =>{
      if(err){
-       return res.status(403).send({message:'Forbined accss'});
+       return res.status(403).send({message:'Forbined access'});
      }
      console.log('decodeed',decoded);
      req.decoded=decoded;
